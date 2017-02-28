7b60d614d2edead18bded8a722ba10c69fd9c499
Pedro Ivo <pivo.bispo@gmail.com>
2017-02-28 17:14:32 +0000

    Refactored index.html through templates

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,11 +2,9 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello, world!</h1>
-<h2>Ipaddress: #{node['ipaddress']}</h2>
-<h2>hostname: #{node['hostname']}</h2>
-"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
 end
 
 service 'httpd' do
